936b07356fc94e68a3880a964ba934252ae607cc
jb_col <jean-baptiste.col@telecom-sudparis.eu>
2022-03-25 09:48:33 +0100

    This is my fisrt commit of la-maison-jungle

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Banner from './Banner'
+import Cart from './Cart'
+import ShoppingList from './ShoppingList'
+import Footer from './Footer'
+
+function App() {
+    const [cart, updateCart] = useState([])
+    
+    return (
+        <div>
+            <Banner>
+            </Banner>
+            <div className='lmj-layout-inner'>
+                <Cart cart={cart} updateCart={updateCart} />
+                <ShoppingList cart={cart} updateCart={updateCart} />
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default App
